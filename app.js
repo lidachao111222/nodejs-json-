a360d994a2c3fff910ab88742b3aa57ba7b273eb
chaoli <chao.li@usask.ca>
2019-12-21 00:47:18 +0800

    首页动态数据展示完成

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const http = require('http'); //搭建服务器
 const path = require('path'); // path模块 ，模块提供了一些用于处理文件路径的小工具
 const urlModel = require('url'); // url模块 ，用于url解析、处理等操作的解决方案。
-const template = require('art-template'); //第三方模块，模板引擎
+const template = require('_art-template@4.13.2@art-template'); //第三方模块，模板引擎
 const fs = require('fs'); //file system 模板， 用于读写数据。
 
 
@@ -40,8 +40,18 @@ app.on('request', (req, res) => {
     //通过请求的 URL 路径来区别不同请求
     if (method == 'GET' && (pathname == '/' || pathname == '/index' || pathname == '/index.html' || pathname == '/favicon.ico ')) {
         //得到html内容传出去。 这里直接使用art-template。不用readfile, 目的是为了后期好封装
-        let content = template(path.join(__dirname, './views/index.html'), {});
-        res.end(content);
+
+        //得到json得到数据
+        fs.readFile(path.join(__dirname, './heros.json'), (err, data) => {
+
+            if (err) return console.log(err.message);
+            //把数据转为对象
+            let arrObj = JSON.parse(data);
+            // 把对象传入的模板引擎中，生成动态数据
+            let content = template(path.join(__dirname, './views/index.html'), arrObj);
+            res.end(content);
+        })
+
     } else if (method == 'GET' && (pathname == '/add' || pathname == '/add.html')) {
         //添加页面的请求
         let content = template(path.join(__dirname, './views/add.html'), {});
@@ -59,8 +69,8 @@ app.on('request', (req, res) => {
         //bootstrap.css的请求
         fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), (err, data) => {
             if (err) return console.log(err.message);
-            res.writeHead(200,{
-                'Content-Type':'text/css; charset=utf8;'
+            res.writeHead(200, {
+                'Content-Type': 'text/css; charset=utf8;'
             })
             res.end(data);
         })
@@ -71,7 +81,7 @@ app.on('request', (req, res) => {
             if (err) return console.log(err.message);
             res.end(data);
         })
-    }else{
+    } else {
         res.end('404');
     }
 
